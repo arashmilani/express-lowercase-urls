2fe7021154dc8b25ab0e1f5d974aac3c16c1e4bb
Arash Milani <me@arashmilani.com>
2015-08-27 10:59:16 +0430

    changed the redirect status code to 301 (Moved Permanently)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next){
   if(req.url !== req.url.toLowerCase()){
-    res.redirect(req.url.toLowerCase());
+    res.redirect(301, req.url.toLowerCase());
   } 
   else{
     next();
